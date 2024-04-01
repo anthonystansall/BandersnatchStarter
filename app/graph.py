60cf4968ed3ccc113c8b1286bb029c1b9d6d0c99
Anthony Stansall <anthony.stansall@gmail.com>
2024-04-01 10:55:25 -0700

    Create notebook for visualizations

--- app/graph.py
+++ app/graph.py
@@ -1,10 +1,20 @@
+"""Module to create visualizations of Monsters collection."""
 from altair import Chart
 
 
 def chart(df, x, y, target) -> Chart:
-    chart = Chart(df).mark_circle().encode(
+    """Creates a scatterplot based on parameters.
+
+    Parameters:
+        df: DataFrame of data to display.
+        x: String name of feature to display on x-axis.
+        y: String name of feature to display on y-axis.
+        target: String name of feature to color code the points by.
+    """
+    scatter = Chart(df).mark_circle().encode(
         x=x,
         y=y,
-        color=target
+        color=target,
+        tooltip=[x, y, target]
     )
-    return chart
+    return scatter
