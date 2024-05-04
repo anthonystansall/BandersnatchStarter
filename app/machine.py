49d5a5eee32faed0e410291744024be8b0da4f65
Anthony Stansall <anthony.stansall@gmail.com>
2024-05-04 13:51:09 -0700

    Add documentation.

--- app/machine.py
+++ app/machine.py
@@ -1,11 +1,16 @@
+"""Module to create and load prediction models."""
+from datetime import datetime
+
+import joblib
 from pandas import DataFrame
 from sklearn.linear_model import LogisticRegression
-from datetime import datetime
-import joblib
+
 
 class Machine:
+    """Class containing prediction model and related info."""
 
     def __init__(self, df: DataFrame):
+        """Initializes LR model with training data."""
         self.name = "Logistic Regression Model"
         target = df["Rarity"]
         features = df.drop(columns=['Rarity'])
@@ -14,18 +19,22 @@ class Machine:
         self.init_time = datetime.now()
 
     def __call__(self, pred_basis: DataFrame):
+        """Makes a prediction based on input features."""
         prediction = self.model.predict(pred_basis)[0]
         confidence = self.model.predict_proba(pred_basis).max(axis=1)[0]
         return prediction, confidence
 
     def save(self, filepath):
+        """Saves the current model."""
         joblib.dump(self, filepath)
 
     @staticmethod
     def open(filepath):
+        """Loads a model from filepath."""
         machine = joblib.load(filepath)
         return machine
 
     def info(self):
-        return f"Base Model: {self.name}<br>TimeStamp: {self.init_time.strftime('%Y-%m-%d %I:%M:%S %p')}"
-        
+        """Returns a string containing information about the model."""
+        return (f"Base Model: {self.name}<br>Timestamp: "
+                f"{self.init_time.strftime('%Y-%m-%d %I:%M:%S %p')}")
